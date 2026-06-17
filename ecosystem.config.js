cd9bb5d01910b4cb86aca37cf0ea9a2750f902f5
desgnpulse <job.muriuki@gmail.com>
2026-06-17 18:02:57 +0300

    chore: ecosystem.config.js reads secrets from .env.local at startup

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,21 @@
+const fs = require("fs")
+const path = require("path")
+
+function loadEnvLocal() {
+  try {
+    const file = fs.readFileSync(path.join(__dirname, ".env.local"), "utf8")
+    return Object.fromEntries(
+      file.split("\n")
+        .filter((l) => l && !l.startsWith("#") && l.includes("="))
+        .map((l) => [l.slice(0, l.indexOf("=")), l.slice(l.indexOf("=") + 1)])
+    )
+  } catch {
+    return {}
+  }
+}
+
+const localEnv = loadEnvLocal()
+
 module.exports = {
   apps: [
     {
@@ -12,6 +30,7 @@ module.exports = {
         HOSTNAME: "0.0.0.0",
         NEXT_PUBLIC_SITE_URL: "https://techtribeafrica.com",
         NEWSLETTER_PUBLICATION_ID: "pub_deeeec18-4bdb-4a58-a47f-ee2a8d07b08c",
+        ...localEnv,
       },
     },
   ],
